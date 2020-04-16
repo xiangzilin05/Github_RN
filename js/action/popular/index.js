25ee389de5b8a07a29d317bbf88e0e1b618f655c
xiangzilin05 <738448953@qq.com>
2020-04-16 21:22:23 +0800

    7-2

---
+++ js/action/popular/index.js
@@ -0,0 +1,23 @@
+import Types from '../types';
+import DataStore from '../../expand/dao/DataStore';
+
+export function onLoadPopularData(storeName, url) {
+  return (dispatch) => {
+    dispatch({
+      type: Types.POPULAR_REFRESH,
+      storeName: storeName,
+    });
+    let dataStore = new DataStore();
+    dataStore.fetchData(url).then((data) => {
+      handleData(dispatch, storeName, data);
+    });
+  };
+}
+
+function handleData(dispatch, storeName, data) {
+  dispatch({
+    type: Types.LOAD_POPULAR_SUCCESS,
+    items: data && data.data && data.data.items,
+    storeName,
+  });
+}
